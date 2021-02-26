95098396b91da1187db30c59950b47a439eb63e2
shibu55 <segusuigao@gmail.com>
2021-02-26 07:45:57 +0000

    relationship

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,21 @@
+class RelationshipsController < ApplicationController
+
+    def create
+      current_user.follow(params[:id])
+      redirect_back(fallback_location: root_path)
+    end
+
+    def destroy
+      current_user.unfollow(params[:id])
+      redirect_back(fallback_location: root_path)
+    end
+
+    def follows
+      user = User.find(params[:id])
+      @users = user.following_user
+    end
+    def followers
+      user = User.find(params[:id])
+      @users = user.follower_user
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,12 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  get 'follows/:id' => 'relationships#follows', as: 'follows' # フォローする
+  get 'followers/:id' => 'relationships#followers', as: 'followers' # フォロー外す
   post 'books/:id', to: 'books#create'
   post 'users', to: 'books#create'
   post 'users/:id', to: 'books#create'
+  post 'follow/:id' => 'relationships#create', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#destroy', as: 'unfollow' # フォロー外す
+  get '/search', to: 'search#search'
 end
